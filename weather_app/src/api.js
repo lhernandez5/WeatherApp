201a06086e4a81f821cf827774871c4dfc8c45c3
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-17 17:48:38 -0500

    Adding api logic

---
+++ weather_app/src/api.js
@@ -0,0 +1,11 @@
+// export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'iu5Id9EUEkmshjOtItux5GMDxSasp15GzrxjsnCLTrUIkE9W6I',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
