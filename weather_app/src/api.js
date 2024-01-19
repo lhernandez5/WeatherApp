73c991114ef76bc732c8a5ce37d3fdefa0a16715
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-18 23:20:08 -0500

    adding logic

--- weather_app/src/api.js
+++ weather_app/src/api.js
@@ -9,3 +9,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/';
+export const WEATHER_API_KEY = 'cfd0798553a135456b283a07bcf87562';
