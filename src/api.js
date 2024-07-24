6c89152e40e1f56fcaa0d486d62dd7e586b2c1fc
Luz Hernandez <hernandezluz18@gmail.com>
2024-07-23 20:45:17 -0400

    added env variables for api keys

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
+const rapidAPIKey = process.env.REACT_APP_RAPID_API_KEY;
+const weatherAPIKey = process.env.REACT_APP_WEATHER_API_KEY;
+
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "iu5Id9EUEkmshjOtItux5GMDxSasp15GzrxjsnCLTrUIkE9W6I",
+    "X-RapidAPI-Key": rapidAPIKey,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
-export const WEATHER_API_KEY = "cfd0798553a135456b283a07bcf87562";
+export const WEATHER_API_KEY = weatherAPIKey;
